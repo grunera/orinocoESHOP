f0b8276650ba4615e62b79c5e4a16bc91e3b3c55
Audrey Gruner <66530333+grunera@users.noreply.github.com>
2020-09-25 16:35:22 +0200

    last commit ag

--- productList.js
+++ productList.js
@@ -1,54 +1,70 @@
-class Product 
-{
-	constructor(pId, pImageUrl, pName, pPrice)
-	{
-	     this.id  = pId;
-         this.imageUrl = pImageUrl;
-         this.name = pName;
-         this.price = pPrice;
-	}
-}   
-
-//A remplacer plus tard par l'utilisation du json
-//const prod1 = new Product(1,);
-//const prod2 = new Product(2,);
-//const prod3 = new Product(3,);
-//const prod4 = new Product(4,"./images/teddy_4.jpg","Gustav",45.00);
-//const prod5 = new Product(5,"./images/teddy_5.jpg","Garfunkel",55.00);
-
-//En dehors de populateTableList3 sinon ne sera pas visible par AjouteLigneProduit
-var listOfProducts = "";
-
-
-//Fonction appelée lors du clic sur le bouton "Show all"
-const populateTableList3=async()=>{
-       
-        
-	const productsResource = await fetch ("http://localhost:3000/api/teddies")
-	const products = await productsResource.json ()
-	console.log (products)
-    	//products.push(prod1,prod2,prod3,prod4,prod5);
-
-      	products.forEach(AjouteLigneProduit);
-
-	document.getElementById("productArray").innerHTML = listOfProducts;
-
-    }
+//lister les produits pour une categorie fixee
+//utiliser fetch
+// créer un affichage pour chaque produit NOM/PHOTO/PRIX
+// fonction recup données + fonction créer affichage
 
 
 
-function AjouteLigneProduit(prod)
-{
-listOfProducts += "<tr class=\"text-center\"><td>"+prod.id+"</td> <td><img src=\"" + prod.imageUrl + "\" class=\"img-fluid img-thumbnail w-50\"></td> <td class=\"w-25 align-middle\">" + prod.name + "</td> <td class=\"w-25 align-middle\">"+ prod.price + "€</td> <td class=\"w-25 align-middle\"><button id="btn btn-primary mt-2 mb-2" onclick="myFunction()";>View</button></td></tr>";  
+async function getProducts(){
+	const response = await fetch('http://localhost:3000/api/teddies')
+    return response.json()
+
 }
 
 
-//Fonction appelée lors du clic sur le bouton "view"
+function displayListProducts(products){
+	const listElement = document.createElement('div')
+	for(let product of products){
+   	displayItemProduct(product,listElement)
 
-function myFunction() {
-    window.open("Detail.html"); 
+
+   }
+   document.getElementById('app').appendChild(listElement)
+
 }
+
+function displayItemProduct(product,listElement){
+	const itemElement = document.createElement('div')
+
+
+
+	itemElement.textContent = product.name;
+	itemElement.imageUrl = product.imageUrl;
+	itemElement._id = product._id;
+	itemElement.price = product.price;
+
+
+    listElement.appendChild(itemElement)
+
+}
+
+  
+async function init(){
+	const products = await getProducts()
+	displayListProducts(products)
+
+
+
+}
+
  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+init()
+
+
 
 
 
